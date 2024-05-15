8905da98ef507a9cff99f9f89050fa4c1ca3a087
faabfab <faab.fab@free.fr>
2024-05-15 17:30:58 +0200

    Compétences

---
+++ src/components/cv/Competences.jsx
@@ -0,0 +1,34 @@
+import competences from "./../../datas/competences.json";
+import "./../../scss/cv/competences.scss";
+
+const Competences = () => {
+  return (
+    <div>
+      <h2>Compétences</h2>
+
+      {competences.map((competence, i) => {
+        return (
+          <div key={i} className="competences">
+            <h3>{competence.name}</h3>
+            {competence.elements.map((element, index) => {
+              return (
+                <div key={index} className="competences_elements">
+                  <p>{element.type}</p>
+                  {element.list.map((item, index) => {
+                    const src =
+                      "https://img.shields.io/badge/" +
+                      item.name +
+                      "-eeeeee?logo=" +
+                      item.badge;
+                    return <img key={index} alt="Static Badge" src={src} />;
+                  })}
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+export default Competences;
